426e4ae9a6e2cce0866dfe3643067af9b568d601
Arie <arie@arie.software>
2023-08-31 00:12:16 +0300

    favorites store

---
+++ src/store/favoritesStore.ts
@@ -0,0 +1,26 @@
+import {atomWithStorage} from "jotai/utils";
+import {atom} from "jotai";
+import {Beer} from "../types";
+
+export const favoritesAtom = atomWithStorage<Array<Beer>>('favorites', []);
+
+/**
+ * Use for adding or removing an item from the user's favorites.
+ */
+export const toggleFavoriteItemAtom = atom(null, (
+  get,
+  set,
+  item: Beer) => {
+  const favoritesArray = get(favoritesAtom);
+  let newFavoritesArray;
+
+  const itemIndex = favoritesArray.findIndex(favoriteItem => favoriteItem.id === item.id);
+
+  if (itemIndex === -1) {
+    newFavoritesArray = [...favoritesArray, item];
+  } else {
+    newFavoritesArray = favoritesArray.filter(favoriteItem => favoriteItem.id !== item.id);
+  }
+
+  set(favoritesAtom, newFavoritesArray);
+});
