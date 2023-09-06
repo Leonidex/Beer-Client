efe0527cc72557627a662cc06421461cb46bdd19
Arie <arie@arie.software>
2023-09-06 19:42:22 +0300

    home list styling

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,7 +2,16 @@ import { useEffect, useState } from "react";
 import { fetchData } from "./utils";
 import { Beer } from "../../types";
 import { Link as RouterLink } from "react-router-dom";
-import { Button, Checkbox, Paper, TextField, Link } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  Paper,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Divider,
+} from "@mui/material";
 import styles from "./Home.module.css";
 import { favoritesAtom } from "../../store/favoritesStore";
 import { useAtomValue } from "jotai";
@@ -17,24 +26,51 @@ const Home = () => {
   return (
     <article>
       <section>
-        <main>
+        <main
+          style={{
+            justifyContent: "space-between",
+          }}
+        >
           <Paper square>
-            <div className={styles.listContainer}>
-              <div className={styles.listHeader}>
-                <TextField label="Filter..." variant="outlined" />
+            <Box className={styles.listContainer}>
+              <Box className={styles.listHeader}>
+                <Typography
+                  variant={"h5"}
+                  sx={{ fontWeight: 600, letterSpacing: 1.5 }}
+                  color="primary"
+                >
+                  A random list of beer breweries:
+                </Typography>
                 <Button variant="contained">Reload list</Button>
-              </div>
-              <ul className={styles.list}>
+              </Box>
+              <Divider />
+              <Grid
+                container
+                columns={{ xs: 4, sm: 4, md: 7, lg: 10 }}
+                width={{ sm: "100%", md: "90%" }}
+                alignSelf={"center"}
+                justifyContent={"center"}
+                gap={[0, 1]}
+              >
                 {beerList.map((beer, index) => (
-                  <li key={index.toString()}>
-                    <Checkbox />
+                  <Grid
+                    item
+                    key={`home-random-beer-list[${index}]`}
+                    xs={4}
+                    sm={3}
+                  >
                     <Link component={RouterLink} to={`/beer/${beer.id}`}>
-                      {beer.name}
+                      <Button
+                        sx={{ width: "100%", textTransform: "none" }}
+                        variant={"outlined"}
+                      >
+                        {beer.name}
+                      </Button>
                     </Link>
-                  </li>
+                  </Grid>
                 ))}
-              </ul>
-            </div>
+              </Grid>
+            </Box>
           </Paper>
 
           <Paper square>
