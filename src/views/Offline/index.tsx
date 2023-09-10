10cb76364a8e0fd138242391289b425bb571823d
Arie <arie@arie.software>
2023-09-10 21:41:33 +0300

    offline

--- src/views/Offline/index.tsx
+++ src/views/Offline/index.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
-import { Tooltip } from "@mui/material";
+import {
+  Box,
+  LinearProgress,
+  Paper,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import SignalWifiBadIcon from "@mui/icons-material/SignalWifiBad";
 
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(true);
@@ -16,12 +24,24 @@ const Offline = () => {
     };
   }, []);
 
-  return isOnline ? null : (
-    <header>
+  return !isOnline ? null : (
+    <Paper square>
       <Tooltip title={"Please check your internet connection"}>
-        <h5 style={{ userSelect: "none" }}>You are offline</h5>
+        <Box display="flex" flexDirection={"column"} width={"100%"}>
+          <Stack direction={"row"} alignItems="center" paddingX={2}>
+            <SignalWifiBadIcon />
+            <Typography
+              style={{ userSelect: "none" }}
+              variant={"h6"}
+              sx={{ padding: 2 }}
+            >
+              You are offline
+            </Typography>
+          </Stack>
+          <LinearProgress />
+        </Box>
       </Tooltip>
-    </header>
+    </Paper>
   );
 };
 
