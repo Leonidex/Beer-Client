013eedbc81ad9b0dbe7618aae775d099a5a17fbf
Arie <arie@arie.software>
2023-09-10 12:15:04 +0300

    beer page details

--- src/views/Beer/index.tsx
+++ src/views/Beer/index.tsx
@@ -2,14 +2,22 @@ import { useEffect, useState } from "react";
 import { Beer as IBeer } from "../../types";
 import { fetchData } from "./utils";
 import { useParams } from "react-router-dom";
-import { toggleFavoriteItemAtom } from "../../store/favoritesStore";
-import { useSetAtom } from "jotai";
+import FavoriteStarButton from "../../components/FavoriteStar";
+import {
+  Box,
+  Divider,
+  Grid,
+  Link,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import styles from "./Beer.module.css";
+import { BreweryLogos } from "../../components/BreweryLogo";
 
 const Beer = () => {
   const { id } = useParams();
   const [beer, setBeer] = useState<IBeer>();
-
-  const toggleFavorite = useSetAtom(toggleFavoriteItemAtom);
 
   // eslint-disable-next-line
   useEffect(fetchData.bind(this, setBeer, id), [id]);
@@ -17,16 +25,87 @@ const Beer = () => {
   return (
     <article>
       <section>
-        <header>
-          <h1>{beer?.name}</h1>
-        </header>
         <main>
-          <span>
-            <b>Type: </b> {beer?.brewery_type}
-          </span>
-          <button onClick={() => toggleFavorite(beer as IBeer)}>
-            Favorite
-          </button>
+          <Paper square sx={{ flexGrow: 1 }}>
+            <Box
+              className={styles.container}
+              sx={{ flex: "0 0 100%", overflowX: "hidden", overflowY: "auto" }}
+            >
+              <Box className={styles.header}>
+                {beer && BreweryLogos.get(beer?.brewery_type)}
+                <Typography
+                  variant={"h5"}
+                  sx={{
+                    fontWeight: 600,
+                    letterSpacing: 1.5,
+                    userSelect: "none",
+                  }}
+                  color="primary"
+                >
+                  &nbsp;{beer?.name}
+                </Typography>
+                <FavoriteStarButton item={beer as IBeer} />
+              </Box>
+              <Divider />
+              <Stack direction={"column"}>
+                <Grid container columns={4} xs={12} md={3} lg={2}>
+                  <Grid container item columns={2} spacing={1}>
+                    <Grid item xs={1}>
+                      <Typography>Type:</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>{beer?.brewery_type}</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>Address:</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>
+                        {beer?.address_1 || beer?.address_2 || beer?.address_3}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>City:</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>{beer?.city}</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>State:</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>{beer?.state}</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>Postal Code:</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>{beer?.postal_code}</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>Country:</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>{beer?.country}</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Typography>Phone:</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                      <Link
+                        href={`tel:${beer?.phone}`}
+                        underline="hover"
+                        color="secondary"
+                      >
+                        <Typography>{beer?.phone}</Typography>
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Box>google maps</Box>
+              </Stack>
+            </Box>
+          </Paper>
         </main>
       </section>
     </article>
